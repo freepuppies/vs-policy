6877a662ada0441e038c29595ddadd53871c7d54
freepuppies <maxined@pm.me>
2023-12-19 18:02:52 -0500

    Add base project files

---
+++ policyModSystem.cs
@@ -0,0 +1,12 @@
+﻿using Vintagestory.API.Common;
+
+[assembly: ModInfo("policy",
+                    Authors = new string[] { "Unknown" },
+                    Description = "This is a sample mod",
+                    Version = "1.0.0")]
+namespace policy
+{
+    public class policyModSystem : ModSystem
+    {
+    }
+}
